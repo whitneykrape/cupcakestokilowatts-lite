7d51cb8bbbe3d686f0bddc3f14594eea845963e6
Whitney Krape <whitney@formfunction.io>
2023-10-27 14:03:28 -0400

    Pulling in theme?

---
+++ cupcakestokilowatts_docs.py
@@ -0,0 +1,28 @@
+"""
+Cupcakes to kiloWatts - Python library for docs.
+
+This is a Python docstring, we can use reStructuredText syntax here!
+
+.. code-block:: python
+
+    # Import cupcakestokilowatts
+    import cupcakestokilowatts
+
+    # Call its only function
+    get_random(kind=["watts"])
+"""
+
+__version__ = "0.1.0"
+
+
+class InvalidKindError(Exception):
+    """Raised if the kind is invalid."""
+
+    pass
+
+
+def get_random(kind=None):
+    """
+    Return a list of random strings.
+    """
+    return ["watts", "kilowatts"]
