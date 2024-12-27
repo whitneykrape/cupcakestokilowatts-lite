d3d7c23020811929eafaae5b5fd1d67d63e7d175
Whitney Krape <whitney@formfunction.io>
2024-12-27 16:16:26 -0500

    Date up date

--- docs/conf.py
+++ docs/conf.py
@@ -3,7 +3,7 @@ master_doc = 'index'
 # -- Project information -----------------------------------------------------
 
 project = "Cupcakes to kiloWatts"
-copyright = "2024 Form Function IO LLC"
+copyright = "2025 Form Function IO LLC"
 author = "Form Function IO LLC"
 
 
